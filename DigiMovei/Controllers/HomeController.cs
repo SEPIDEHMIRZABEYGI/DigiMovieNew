8ef498aff380ad41b5c36d3aa78b3b9d8e29fe3a
sepideh <sepideh.mirzabeygi@gmail.com>
2017-09-09 17:53:42 +0430

    Add project files.

---
+++ DigiMovei/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiMovei.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About( int? id)
+        {
+
+            return Content(id.ToString());
+            //ViewBag.Message = "Your application description page.";
+
+            //return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DigiMovei/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigiMovei.Startup))]
+namespace DigiMovei
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
