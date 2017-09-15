6f2e170936805917c022f287ce4edb9c89a39dcb
sepideh <sepideh.mirzabeygi@gmail.com>
2017-09-15 10:43:19 +0430

    p10

--- DigiMovei/Controllers/HomeController.cs
+++ DigiMovei/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net.Mail;
 using System.Web.Mvc;
+using DigiMovei.Helper;
 
 namespace DigiMovei.Controllers
 {
@@ -29,6 +31,39 @@ namespace DigiMovei.Controllers
         }
 
 
-    
+
+        public ActionResult SendEmail()
+        {
+            var message = new MailMessage
+            {
+                From = Email.GetMailAddress(EmailType.Info),
+                Subject = "وب سایت دیجی مووی - بخش ارتباط با ما",
+                Body = string.Format("نام : " +
+                "{0}<hr>" +
+                "ایمیل : " +
+                "{1}<hr>" +
+                "موضوع : " +
+                "{2}<hr>" +
+                "پیام : " +
+                "{3}", Request.Form["name"], Request.Form["email"], Request.Form["subject"], Request.Form["body"]),
+                IsBodyHtml = true
+            };
+
+            message.To.Add(Email.GetMailName(EmailType.Info));
+            var smtp = Email.GetSmtp(EmailType.Info);
+
+            try
+            {
+                smtp.Send(message);
+                ViewBag.Status = 1;
+            }
+            catch
+            {
+                ViewBag.Status = 0;
+            }
+
+            return View("Contact");
+        }
+
     }
 }
---
+++ DigiMovei/Helper/Email.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+
+namespace DigiMovei.Helper
+{
+    public enum EmailType { Empty, Info }
+
+    public class Email
+    {
+        public static string GetMailName(EmailType type)
+        {
+            if (type == EmailType.Empty)
+                return "";
+            else
+                return "sepideh.mirzabeygi@gmail.com";
+        }
+
+        public static MailAddress GetMailAddress(EmailType type)
+        {
+            if (type == EmailType.Empty)
+                return new MailAddress("");
+            else
+                return new MailAddress("sepideh.mirzabeygi@gmail.com", "دیجی مووی");
+        }
+
+        public static SmtpClient GetSmtp(EmailType type)
+        {
+            if (type == EmailType.Empty)
+                return new SmtpClient();
+            else
+                return new SmtpClient
+                {
+                    Host = "sepideh.mirzabeygi@gmail.com",
+                    Port = 587,
+                    UseDefaultCredentials = false,
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential("sepideh.mirzabeygi@gmail.com", "410se410")
+                };
+        }
+    }
+}
